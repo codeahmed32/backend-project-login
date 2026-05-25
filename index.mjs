3e1a26438678dd6d339762288bdaf72093db8b97
Ahmed Abdullah <abdullahahmad6782@gmail.com>
2026-05-25 05:59:37 +0500

    cors resolving

--- index.mjs
+++ index.mjs
@@ -11,7 +11,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-    origin: "https://login-signup-project-live.netlify.app/login",
+    origin: "https://login-signup-project-live.netlify.app",
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"]
 }));
